c73a49c43b3ea9b29027c0e64971c80f73d60c73
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2026-03-21 02:39:25 +0530

    proper load fade

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
 import ProjectDetail from './pages/ProjectDetail'
+import LoadingScreen from './components/LoadingScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <LoadingScreen />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
